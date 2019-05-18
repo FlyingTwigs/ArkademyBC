a697d0e06df65e76d067876356942c50c21455a4
TheFaR8 <dorerudy@gmail.com>
2019-05-18 19:53:02 +0700

    Update logic

--- 2.py
+++ 2.py
@@ -2,14 +2,25 @@ import random
 import string
 
 array = []
+result = []
 lettersAndDigits = string.ascii_letters + string.digits
+
 
 def generateString(number_generated):
     for i in range(number_generated):
         art = ""
         stringLength=32
-        art += ''.join(random.sample(lettersAndDigits,stringLength))
+        art += ''.join(random.choice(lettersAndDigits) for i in range(stringLength))
         array.append(art)
     return array
 
-print(generateString(3))
+
+def uniqueCharacter():
+    for y in array:
+        if y not in result:
+            result.append(y)
+    return result
+
+
+generateString(3)
+print(uniqueCharacter)
