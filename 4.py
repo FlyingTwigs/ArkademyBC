2cb007468969b47b02061a84aad66f519c09cc5c
TheFaR8 <dorerudy@gmail.com>
2019-05-18 16:37:28 +0700

    Add all

---
+++ 4.py
@@ -0,0 +1,20 @@
+def cetakGambar(banyakGambar):
+    result=""
+
+    for baris in range(banyakGambar):
+        for kolom in range(banyakGambar):
+            if (baris==kolom or kolom == banyakGambar - baris - 1):
+<<<<<<< HEAD
+                result+="X "
+            else:
+                result+="= "
+=======
+                result+="X"
+            else:
+                result+="="
+>>>>>>> f68572953e4e6e50a706ccc42f5d81dcfa5da71e
+        result+="\n"
+
+    return result
+
+print(cetakGambar(7))
